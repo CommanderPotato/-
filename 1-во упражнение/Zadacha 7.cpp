9b88e9a0d63ee3db05ec80059c697afaa9e6a8ab
Мартин <martin.totev@abv.bg>
2014-11-25 17:57:21 +0200

    Revert "Задачи -30.10"
    This reverts commit 37730ed2467e957ca0642a99206c2f5623a7bfd4.

---
+++ 1-во упражнение/Zadacha 7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	double b, d;
+	cout << "Vyvedete cenata na produktite" << endl;
+	cin >> b;
+	cout << "Vyvedete broqt na produktite" << endl;
+	cin >> a;
+	d = (a + b) / 5;
+	cout << "DDS-to vi e " << d << endl;
+	system("pause");
+	return 0;
+}
